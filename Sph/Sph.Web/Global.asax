426b526fe510ad9ac99a58438544f54a43a72518
DerHulk <thegrayhulk@googlemail.com>
2014-11-10 21:33:31 +0100

    Init Project.

---
+++ Sph/Sph.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sph.Web.MvcApplication" Language="C#" %>
---
+++ Sph/Sph.Web/Scripts/Game.js
@@ -0,0 +1,19 @@
+﻿var SimpleGame = (function () {
+    function SimpleGame() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+    SimpleGame.prototype.preload = function () {
+        this.game.load.image('logo', './Content/images/SphIcon.png');
+    };
+
+    SimpleGame.prototype.create = function () {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    };
+    return SimpleGame;
+})();
+
+window.onload = function () {
+    var game = new SimpleGame();
+};
+//# sourceMappingURL=game.js.map
